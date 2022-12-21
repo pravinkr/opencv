8a0def96379e225a3c03a49d40614288fd78147c
Pravin Kumar <pravin.kr@gmail.com>
2022-12-21 13:54:57 +0530

    Add files via upload

---
+++ multi-usb-camera.py
@@ -0,0 +1,172 @@
+# imports
+import os,sys,time
+import numpy as np
+import cv2
+
+# init camera
+#camera = cv2.VideoCapture(1) ### <<<=== SET THE CORRECT CAMERA NUMBER
+camera_left = cv2.VideoCapture("/dev/video2") ### Left USB Camera
+camera_right = cv2.VideoCapture("/dev/video4") ### Right USB Camera
+
+camera_left.set(3,640)             # set frame width
+camera_left.set(4,370)              # set frame height
+
+camera_right.set(3,640)             # set frame width
+camera_right.set(4,370)              # set frame height
+
+
+time.sleep(0.5)
+
+print(camera_left.get(3),camera_left.get(4))
+print(camera_left.get(5))
+
+print(camera_left.get(3),camera_left.get(4))
+print(camera_left.get(5))
+
+print(camera_right.get(3),camera_right.get(4))
+print(camera_right.get(5))
+
+print(camera_right.get(3),camera_right.get(4))
+print(camera_right.get(5))
+
+
+
+# master frame
+master = None
+
+while 1:
+
+    # grab a frame
+    (grabbed_left,frame0_left) = camera_left.read()
+    
+    # end of feed
+    if not grabbed_left:
+        print("No left frame detected")
+        break
+
+    # grab a frame
+    (grabbed_right,frame0_right) = camera_right.read()
+    
+    # end of feed
+    if not grabbed_right:
+        print("No right frame detected")
+        break
+
+    # gray frame
+    # frame1 = cv2.cvtColor(frame0,cv2.COLOR_BGR2GRAY)
+
+    # # blur frame
+    # frame2 = cv2.GaussianBlur(frame1,(15,15),0)
+
+    # # initialize master
+    # if master is None:
+    #     master = frame2
+    #     continue
+
+    # # delta frame
+    # frame3 = cv2.absdiff(master,frame2)
+
+    # # threshold frame
+    # frame4 = cv2.threshold(frame3,15,255,cv2.THRESH_BINARY)[1]
+
+    # # dilate the thresholded image to fill in holes
+    # kernel = np.ones((2,2),np.uint8)
+    # frame5 = cv2.erode(frame4,kernel,iterations=4)
+    # frame5 = cv2.dilate(frame5,kernel,iterations=8)
+
+    # # find contours on thresholded image
+    # contours,nada = cv2.findContours(frame5.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    # # make coutour frame
+    # frame6 = frame0.copy()
+
+    # # target contours
+    # targets = []
+
+    # # loop over the contours
+    # for c in contours:
+        
+    #     # if the contour is too small, ignore it
+    #     if cv2.contourArea(c) < 500:
+    #             continue
+
+    #     # contour data
+    #     M = cv2.moments(c)#;print( M )
+    #     cx = int(M['m10']/M['m00'])
+    #     cy = int(M['m01']/M['m00'])
+    #     x,y,w,h = cv2.boundingRect(c)
+    #     rx = x+int(w/2)
+    #     ry = y+int(h/2)
+    #     ca = cv2.contourArea(c)
+
+    #     # plot contours
+    #     cv2.drawContours(frame6,[c],0,(0,0,255),2)
+    #     cv2.rectangle(frame6,(x,y),(x+w,y+h),(0,255,0),2)
+    #     cv2.circle(frame6,(cx,cy),2,(0,0,255),2)
+    #     cv2.circle(frame6,(rx,ry),2,(0,255,0),2)
+
+    #     # save target contours
+    #     targets.append((cx,cy,ca))
+
+    # # make target
+    # mx = 0
+    # my = 0
+    # if targets:
+        
+    #     # average centroid adjusted for contour size
+    #     #area = 0
+    #     #for x,y,a in targets:
+    #     #    mx += x*a
+    #     #    my += y*a
+    #     #    area += a
+    #     #mx = int(round(mx/area,0))
+    #     #my = int(round(my/area,0))
+
+    #     # centroid of largest contour
+    #     area = 0
+    #     for x,y,a in targets:
+    #         if a > area:
+    #             mx = x
+    #             my = y
+    #             area = a
+        
+
+    # # plot target
+    # tr = 50
+    # frame7 = frame0.copy()
+    # if targets:
+    #     cv2.circle(frame7,(mx,my),tr,(0,0,255,0),2)
+    #     cv2.line(frame7,(mx-tr,my),(mx+tr,my),(0,0,255,0),2)
+    #     cv2.line(frame7,(mx,my-tr),(mx,my+tr),(0,0,255,0),2)
+    
+    # # update master
+    # master = frame2
+
+    # display
+    cv2.imshow("Frame0_left: Raw",frame0_left)
+    cv2.imshow("Frame0_right: Raw",frame0_right)
+
+ 
+ 
+    # cv2.imshow("Frame1: Gray",frame1)
+    # cv2.imshow("Frame2: Blur",frame2)
+    # cv2.imshow("Frame3: Delta",frame3)
+    # cv2.imshow("Frame4: Threshold",frame4)
+    # cv2.imshow("Frame5: Dialated",frame5)
+    # cv2.imshow("Frame6: Contours",frame6)
+    # cv2.imshow("Frame7: Target",frame7)
+    
+    # key delay and action
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+    elif key != 255:
+        print('key:',[chr(key)])
+
+# release camera
+camera_left.release()
+camera_right.release()
+
+
+# close all windows
+cv2.destroyAllWindows()
